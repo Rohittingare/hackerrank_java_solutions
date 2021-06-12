9d6af0edd383dffa48799638a24190fd743191d3
Rohit Tingare <52101615+Rohittingare@users.noreply.github.com>
2021-06-12 16:38:09 +0530

    Update and rename phonebook to phonebook.java

--- phonebook.java
+++ phonebook.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.io.*;
 
-class Solution{
+class phonebook{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
